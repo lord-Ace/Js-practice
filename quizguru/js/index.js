4b87cd210efc5c565985d3f025fc5176025bf0e2
chukwufumnaya Ashinze <emmanuelashinze612@gmail.com>
2025-10-16 11:53:16 +0100

    created quiz guru

--- quizguru/js/index.js
+++ quizguru/js/index.js
@@ -1,24 +1,65 @@
 document.addEventListener('DOMContentLoaded', function(){
-  const stage = document.querySelectorAll('.stage')
-  const next = document.querySelectorAll('.continue')
+  const data = document.querySelectorAll('.data')
   const dialog = document.getElementById('dialog')
+  const begin = document.getElementById('begin')
   const user = localStorage.getItem('user')//gets the name of the currentuser
+  // user data and game info
+  let gameData = {
+    name: user?.trim() || ' ',
+    difficulty: ' ',
+    duration : {
+     minutes: '0',
+     seconds: '10' 
+    }
+  }
 
-    // localStorage.clear()
-// localStorage.setItem('user', 'blu')
-  if (user == '' || user == null){
-    stage[0].classList.add('active')
-    console.log(`no current user`)
+    localStorage.clear()
+  if (!user){
+    data[0].classList.add('active')
   }else{
-    console.log(user)
-    dialog.showModal()
-    stage[1].classList.add('active')
+    data[1].classList.add('active')
   }
   
-  next.forEach(button=>{
-    button.addEventListener('click', function(){
-      const previous = this.previousElementSibling
-      console.log(this.getAttribute('id'))
+  data.forEach((button, index)=>{
+    button.addEventListener('submit', function(e){
+      e.preventDefault()
+      this.classList.remove('active')
+      if (index+1 != 3){
+      data[index+1].classList.add('active')
+      }else{
+      dialog.showModal()
+      }
+      const form = new FormData(this)
+      form.forEach((entry)=>{
+        console.log(it)
+      if(gameData?.[name]){
+        gameData[name]=entry
+        if(name === 'name'){
+          localStorage.setItem('user', entry)
+        }
+      }else{
+        gameData.duration[name]=entry
+      }
+      })
     })
+  })
+  
+  // const {name, difficulty} = gameData
+  // let {minutes, seconds} = gameData.duration
+  console.log(name)
+  function countDown(){
+    seconds --
+    gameData.duration.seconds = secs
+    if (seconds == 0){
+      seconds = 59
+      minutes--
+    }
+    if(seconds == 0 && minutes-- == 0){
+      console.log('hurray')
+    }
+  }
+  begin.addEventListener('click', function(){
+    dialog.close()
+    setInterval(() => {countDown()}, 1000);
   })
 })
